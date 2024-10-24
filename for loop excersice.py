2eaff590df9cac11c344aa4436f2611be70ec8b2
Carlos123-a11y <20cMendezAlma.acc@lwetb.ie>
2024-10-24 12:00:23 +0100

    Add files via upload

---
+++ for loop excersice.py
@@ -0,0 +1,37 @@
+#1
+'''
+side1=int(input('enter a number:'))
+side2=int(input('enter a number:'))
+side3=int(input('enter a number:'))
+if side1==side2==side3:
+    print('equialateral')
+elif side1!=side2:
+    print('scalene')
+else:
+    print('isoscles')
+  
+#2
+user=int(input('enter the number 4 or 8:'))
+if user%10==4:
+    print('ends with four')
+if user%10==8:
+    print('ends with eight')
+else:
+    print('ends with neither')
+#3
+
+usernumber=int(input('enter a number:'))
+for usernumber in range(11,21):
+    print(usernumber)
+    if usernumber%3==0:
+        print('tipsy topsy')
+    elif usernumber%3==0:
+        print('Tipsy')
+    elif usernumber%7==0:
+        print('Topsy')
+'''
+#4
+
+
+
+
