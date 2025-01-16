9633b763138b10be064d6c715953ed3be8ddc680
Carlos123-a11y <20cMendezAlma.acc@lwetb.ie>
2025-01-16 11:58:17 +0000

    Add files via upload

---
+++ if school.py
@@ -0,0 +1,24 @@
+#2
+'''
+tempno=int(input('enter a number:'))
+temp=tempno
+if temp < 32:
+    print('ice')
+elif temp <212:
+    print('water')
+else:
+    print('steam')
+#3
+'''
+user=int(input('enter a number:'))
+x=1
+if x > 3:
+    if x > 4:
+        print("A",end='')
+    else:
+        print("B",end='')
+elif x<2:
+        if(x!=0):
+            print("C",end='')
+        print("D")
+#4
