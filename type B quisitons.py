9f3b086ef91b8b571a9d700d18a884e61fd0b44e
Carlos123-a11y <20cMendezAlma.acc@lwetb.ie>
2025-01-21 13:39:55 +0000

    Add files via upload

---
+++ type B quisitons.py
@@ -0,0 +1,37 @@
+#Q1
+#(A)
+print('''
+ 1
+  2
+   3
+''')
+
+#(B)
+text='Test./nNext line.'
+print(text)
+
+#(C)
+print('one','two'*2)
+print('one'+'two'*2)
+print(len('0123456789'))
+
+#(D)
+s='0123456789'
+print(s[:3],','',',s[0:3],'-',s[2:5])
+print(s[:3],'-',s[3],',',s[3:100])
+print(s[-100:-3],s[-100:3])
+
+
+#(E)
+s='987654321'
+print(s[-1],s[-3])
+print(s[-3],s[-1])
+print(s[-100:-3],s[-100:3])
+
+#Q2
+#(A)
+y=str(123)
+x='hello'*3
+print(x,y)
+
+#(B)
