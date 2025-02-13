5f6db1fd7ab45940899b29548c625b123886a673
Carlos123-a11y <20cMendezAlma.acc@lwetb.ie>
2025-02-13 11:59:54 +0000

    Add files via upload

---
+++ midterm hew.py
@@ -0,0 +1,27 @@
+#page 223 electronic copy questions
+#question 1
+'''
+a=input('enter a list:')
+list_of_input=list(a)
+list_of_input.reverse()
+print(list_of_input)
+
+
+
+#question2
+lst1=[input('enter a word:')]  
+lst2=[input('enter a word too add it to the first list:')]
+lst3=lst1+lst2
+print(lst3)
+#3
+
+list2=input('enter a list of 1-12:')
+lista=list(list2)
+lenght=len(list2)
+for i in range(0,lenght):
+    if i >10:
+        lista[i]=10
+        
+print(lista)
+'''
+#4
