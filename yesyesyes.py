630d984f1f0695fd5ad05aac9016b1c27355f87b
Carlos123-a11y <20cMendezAlma.acc@lwetb.ie>
2025-03-04 13:41:43 +0000

    Add files via upload

---
+++ yesyesyes.py
@@ -0,0 +1,62 @@
+row1=['1','2','3']
+row2=['4','5','6']
+row3=['7','8','9']
+player1=input(' player1 enter a number:')
+player2=input(' player2 enter a number:')
+row1[0=='']
+if player1=='1':
+    row1[0]='x'
+    print(row1,'\n',row2,'\n',row3)
+if player1=='2':
+    row1[1]='x'
+    print(row1,'\n',row2,'\n',row3)
+if player1=='3':
+    row1[2]='x'
+    print(row1,'\n',row2,'\n',row3)
+if player1=='4':
+    row2[0]='x'
+    print(row1,'\n',row2,'\n',row3)
+if player1=='1':
+    row2[1]='x'
+    print(row1,'\n',row2,'\n',row3)
+if player1=='1':
+    row2[2]='x'
+    print(row1,'\n',row2,'\n',row3)
+if player1=='1':
+    row3[0]='x'
+    print(row1,'\n',row2,'\n',row3)
+if player1=='1':
+    row3[1]='x'
+    print(row1,'\n',row2,'\n',row3)
+if player1=='1':
+    row3[2]='x'
+    print(row1,'\n',row2,'\n',row3)
+    
+
+if player2=='1':
+    row1[0]='O'
+    print(row1,'\n',row2,'\n',row3)
+if player2=='2':
+    row1[1]='O'
+    print(row1,'\n',row2,'\n',row3)
+if player2=='3':
+    row1[2]='O'
+    print(row1,'\n',row2,'\n',row3)
+if player2=='4':
+    row2[0]='O'
+    print(row1,'\n',row2,'\n',row3)
+if player2=='1':
+    row2[1]='O'
+    print(row1,'\n',row2,'\n',row3)
+if player2=='1':
+    row2[2]='O'
+    print(row1,'\n',row2,'\n',row3)
+if player2=='1':
+    row3[0]='O'
+    print(row1,'\n',row2,'\n',row3)
+if player2=='1':
+    row3[1]='O'
+    print(row1,'\n',row2,'\n',row3)
+if player2=='1':
+    row3[2]='O'
+    print(row1,'\n',row2,'\n',row3)
