a34d05d10ab7fd786ed938b84fbc6c06fcdb5f06
Carlos123-a11y <20cMendezAlma.acc@lwetb.ie>
2025-05-14 11:59:37 +0100

    Add files via upload

---
+++ questions 1232132113213213213.py
@@ -0,0 +1,11 @@
+phone=[1,2,3,4,5,6,7,8,9,0]
+user=input('enter your phone number:')
+
+if len(user) ==12:
+    if user[0:3].isdigit() and user[:].isdigit() and user[:].isdigit() and user[] == '-' and user[] == '-':
+        print('valid number')
+    else:
+        print('invalid number')
+    
+    
+    print(user)
